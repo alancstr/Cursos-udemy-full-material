93fe1b57e371ecabdd66efaf56afc23ed2bd51e1
Moises Castro <moisescastrocas@gmail.com>
2024-09-03 20:30:21 -0600

    Añadiendo material de c sharp

---
+++ C#/MiPrimeraAplicacionConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiPrimeraAplicacionConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World");
+            //Console.ReadKey();
+        }
+    }
+}
